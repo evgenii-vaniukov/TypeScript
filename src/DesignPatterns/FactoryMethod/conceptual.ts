b1d06f7b1ba3807548e6d83a9b18bdacf9354bc4
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2024-03-25 12:25:09 +0200

    Factory review.

--- src/DesignPatterns/FactoryMethod/conceptual.ts
+++ src/DesignPatterns/FactoryMethod/conceptual.ts
@@ -1,31 +1,44 @@
 interface Product {
-  operation(): void;
+  operation(): string; /// Some functionality that makes sense for every concrete inplementation of the interface.
 }
 
 class ConcreteProduct1 implements Product {
-  public operation() {
-    console.log("ConcreteProduct1");
+  public operation(): string {
+    return "ConcreteProduct1";
   }
 }
-
 class ConcreteProduct2 implements Product {
-  public operation() {
-    console.log("ConcreteProduct2");
+  public operation(): string {
+    return "ConcreteProduct2";
   }
 }
 
 abstract class Creator {
   public abstract factoryMethod(): Product;
+
+  public someOperation(): string {
+    const product = this.factoryMethod();
+    return `Hello from ${product.operation()}`;
+  }
 }
 
-class ConcreteCreator1 extends Creator {
+class ConcretCreator1 extends Creator {
   public factoryMethod(): Product {
     return new ConcreteProduct1();
   }
 }
 
-function clientCode(creator: Creator) {
-  console.log(
-    `Client: I'm not aware of the creator's class, but it still works with ${creator} .`
-  );
+class ConcretCreator2 extends Creator {
+  public factoryMethod(): Product {
+    return new ConcreteProduct2();
+  }
 }
+
+/// Client code.
+function clientFunction(creator: Creator): void {
+  console.log(creator.someOperation());
+}
+
+const creator = new ConcretCreator1();
+
+clientFunction(creator);
